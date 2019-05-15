2253b0e8abdeb15a4b6a6345d8b7de0adecf2aec
gianglt97 <legiangpx9751@gmail.com>
2019-05-15 20:32:47 +0700

    unit 3

---
+++ users.php
@@ -0,0 +1,23 @@
+<?php
+	require_once ("header.php");
+?>
+<div id="content-wrapper">
+
+      <div class="container-fluid">
+
+        <!-- Breadcrumbs-->
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item">
+            <a href="index.html">Dashboard</a>
+          </li>
+          <li class="breadcrumb-item active">Users</li>
+        </ol>
+
+        <!-- Page Content -->
+        <h1>Danh sách nhân viên</h1>
+        
+      </div>
+      <!-- /.container-fluid -->
+<?php
+	require_once ("footer.php");
+?>
