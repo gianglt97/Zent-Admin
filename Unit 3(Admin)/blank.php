de171d0c27459463ecff384d669fa7569bbe4c00
gianglt97 <legiangpx9751@gmail.com>
2019-05-15 21:33:05 +0700

    Unit3(Admin)

---
+++ Unit 3(Admin)/blank.php
@@ -0,0 +1,26 @@
+<?php
+	require_once("header.php");
+?>
+<div id="content-wrapper">
+
+      <div class="container-fluid">
+
+        <!-- Breadcrumbs-->
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item">
+            <a href="index.php">Bảng điều khiển</a>
+          </li>
+          <li class="breadcrumb-item active">Blank Page</li>
+        </ol>
+
+        <!-- Page Content -->
+        <h1>Blank Page</h1>
+        
+
+      </div>
+      <!-- /.container-fluid -->
+
+
+<?php
+	require_once("footer.php");
+?>
---
+++ Unit 3(Admin)/posts.php
@@ -0,0 +1,23 @@
+<?php
+require_once ("header.php");
+?>
+<div id="content-wrapper">
+
+      <div class="container-fluid">
+
+        <!-- Breadcrumbs-->
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item">
+            <a href="index.php">Bảng điều khiển</a>
+          </li>
+          <li class="breadcrumb-item active">Bài viết</li>
+        </ol>
+
+        <!-- Page Content -->
+        <h1>QUản lý bài viết</h1>
+        
+      </div>
+      <!-- /.container-fluid -->
+<?php
+require_once ("footer.php");
+?>
